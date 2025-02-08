b3b09e90ed36203501b076007a249f54155caf75
Happyyadav007 <hy249796@gmail.com>
2025-02-08 11:55:41 +0530

    forth commit

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
-const API_BASE_URL = "https://mailsender-server.onrender.com";
+const API_BASE_URL = "https://mailsender-server.onrender.com/api/v1";
+//const API_BASE_URL = "http://localhost:5000/api/v1"; 
+
 export default API_BASE_URL;
